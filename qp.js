c3bac2d6104f657c23bc58f4a60ebf5e2a76bc32
Debashis Kar <116684191+debashis99374@users.noreply.github.com>
2022-11-24 23:50:30 +0530

    Update qp.js
    final

--- qp.js
+++ qp.js
@@ -1,45 +1,67 @@
-var score=0;
-var userName=prompt('what is your name?= ')
-console.log('Welcome '+userName+' to Debashis quiz')
-console.log('---------------------------------------------')
+var score = 0;
+var response = "";
+var userName = prompt("May i ask your name sir?= ");
+console.log("Welcome to my personal Quiz " + userName + " sir");
+console.log("lets go....")
 
-function play(question,answer) {
-  var userAnswer=prompt(question);
-  if (userAnswer===answer){
-    console.log('correct')
-    score+=1
-  }else{
-    console.log('wrong')
+function play(question, answer) {
+  var UserAnswer =prompt(question);
+  if (UserAnswer === answer) {
+    score += 1;
+    response = "Right";
+  } else {
+    Response = "Wrong";
   }
-  console.log(score)
-  console.log('---------------')
-  
+  console.log(response);
+  console.log("score is = " + score);
+  console.log("---------------------");
 }
- var questionOne={
-  question:'what is my nick name= ',
-  answer:'gugula'
+var Question1 = {
+  question: "What is my name?",
+  answer: "Debashis"
 };
-var questionTwo={
-  question:'whats my hobby= ',
-  answer:'coading'
+var Question2 = {
+  question: "What is my age?",
+  answer: "22"
 };
-var questionThree={
-  question:'what i like to eat= ',
-  answer:'sahi panner'
+var Question3 = {
+  question: "What is my hight?",
+  answer: "6 Feet"
 };
-var questionFour={
-  question:'Am i a lazy person= ',
-  answer:'maybe'
+var Question4 = {
+  question: "Am i a Vegan?",
+  answer: "No"
 };
-var questionFive={
-  question:'Do i like you=',
-  answer:'yes'
+var Question5 = {
+  question: "What is my favourate colour?",
+  answer: "Green"
 };
-var a=[questionOne,questionTwo,questionThree,questionFour,questionFive];
-for(let i=0;i<a.length;i++){
-var currentQuestion=a[i];
-  play(currentQuestion.question,currentQuestion.answer)
+var Question6 = {
+  question: "Do i love Meditation?",
+  answer: "Yes"
+};
+var Question7 = {
+  question: "Cricket or Football?",
+  answer: "Football"
+};
+var Question8 = {
+  question: "Guess my nickname?",
+  answer: "Dev"
+};
+var Question9 = {
+  question: "Who is my idol?",
+  answer: "Me"
+};
+var Question10 = {
+  question: "Do i love you?",
+  answer: "Yes"
+};
+
+var listOfQuestions = [Question1, Question2, Question3, Question4, Question5, Question6, Question7, Question8, Question9, Question10];
+for (let i = 0; i < listOfQuestions.length; i++) {
+  play(listOfQuestions[i].question, listOfQuestions[i].answer);
 }
-console.log('---------------')
-console.log('thank you for your time   ')
-console.log('have a nice day!!')
+
+console.log("Thank you for your valuable time " + userName + " sir");
+console.log("Have a nice day!!!");
+console.log("----------------------------------------END-------------------------------------------------------------------");
